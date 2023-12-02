8fe37a4919922ce4f2e438233dca3a8b6b2067e5
Hleb Drazdou <Hleb.Drazdou@gmail.com>
2023-12-02 22:32:01 +0100

    created first script and cell prefab

---
+++ Assets/Scripts/GameGrid.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GameGrid : MonoBehaviour
+{
+    #region Unity lifecycle
+
+    private void Start()
+    {
+        FillInGrid();
+    }
+
+    private void Update() { }
+
+    #endregion
+
+    #region Private methods
+
+    private void FillInGrid()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    #endregion
+}
